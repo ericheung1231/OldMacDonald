fc337dcfbdd86f587e2d68d7e336559af6a094d4
unknown <ercheung3@697-334-Dale.sf.k12.internal>
2014-10-27 12:16:24 -0700

    working

--- build-tmp/source/OldMacDonald.java
+++ build-tmp/source/OldMacDonald.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OldMacDonald extends PApplet {
+
 //your variable declarations here
 public void setup() 
 {   /*  
@@ -126,4 +142,13 @@ class NameCow extends Cow
   public String getSound(){return mySound;}     
   public String getType(){return myType;}
   public String getName(){return myName;} 
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OldMacDonald" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
